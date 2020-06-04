6613524b1d046a726387f4c6bf7cc116bb1ee6d1
tn101 <66131777+tn101@users.noreply.github.com>
2020-06-04 15:49:26 +0200

    Add files via upload

---
+++ tribal-networks-website/scripts/index-scripts/dropdown_menu.js
@@ -0,0 +1,13 @@
+function dropdownMenu() {
+	 var x = document.getElementById("dropdownClick");
+	 if (x.className === "topnav") {
+	 	x.className += " responsive";
+	 	/*chnage topnav to topnave.responsive*/
+	 }  else {
+	 	x.className = "topnav";
+	 }
+
+}
+
+
+    
